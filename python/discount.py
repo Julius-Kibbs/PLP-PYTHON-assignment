5d5e4b77adbf12a4b95e611b4dc3adde2ecb09bd
Julius Kibbassa <jjkibbassa@gmail.com>
2025-08-22 02:13:04 +0300

    pushing code to github

--- python/discount.py
+++ python/discount.py
@@ -12,4 +12,4 @@ original_price = float(input("Enter the original price of the item: "))
 discount_percentage = float(input("Enter the discount percentage: "))
 
 discount_price = calculate_discount(original_price, discount_percentage)
-print(f"The final price after applying discount is: Tshs{discount_price:.2f}")
+print(f"The final price after applying the discount is: Tshs{discount_price:.2f}")
