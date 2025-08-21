0628a8b143cc83b871e30b9ccf32cde49831b2a3
Julius Kibbassa <jjkibbassa@gmail.com>
2025-08-22 02:12:10 +0300

    finished discount code change

---
+++ python/discount.py
@@ -0,0 +1,15 @@
+# this is the fucntion responsible for calculating the discount on an item
+def calculate_discount(price, discount_percentage):
+    if discount_percentage >= 20:
+        discount_amount = price * (discount_percentage / 100)
+        final_price = price - discount_amount
+        return final_price
+    else :
+        return price
+
+# this is where we take input from the user
+original_price = float(input("Enter the original price of the item: "))
+discount_percentage = float(input("Enter the discount percentage: "))
+
+discount_price = calculate_discount(original_price, discount_percentage)
+print(f"The final price after applying discount is: Tshs{discount_price:.2f}")
